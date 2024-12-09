656a2647f286d6bad0b73cd3a6bac4e7d6919bee
DiazPermana <khafriza.permana@mhs.unsoed.ac.id>
2024-12-09 18:07:48 +0700

    add main

---
+++ main.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>main</title>
+</head>
+<body>
+    <div class="logo">
+        <img src="assets/logo.jpg" alt="">
+    </div>
+    <div class="navbar">
+        <a href="">The Game</a>
+        <a href="">Gameplay</a>
+        <a href="">Profile</a>
+    </div>
+    <div class="main">
+        <div class="maintext">
+            <img src="assets/logo.jpg" alt="logo">
+            <p>Undertale adalah game RPG 2D yang dikembangkan oleh Toby Fox, di mana pemain mengendalikan seorang anak yang jatuh ke dunia bawah tanah (Underground), sebuah tempat terpencil yang dihuni oleh monster. Untuk kembali ke permukaan, pemain harus menjelajahi berbagai area, berinteraksi dengan karakter unik, dan membuat keputusan yang memengaruhi jalannya cerita.
+
+Game ini dikenal dengan sistem pertarungan inovatifnya, yang memungkinkan pemain untuk memilih bertarung atau menyelesaikan konflik secara damai. Pilihan ini menentukan jalur cerita dan ending, termasuk jalur Pacifist, Neutral, dan Genocide. Undertale mengeksplorasi tema besar seperti empati, konsekuensi pilihan, dan moralitas, dengan humor, emosi, dan musik ikonis yang membuatnya menjadi salah satu game indie terbaik sepanjang masa.</p>
+            <h6>The 3 ending of undertale</h6>
+            <input type="submit" class="submit-button" value="PACIFIST">
+            <input type="submit" class="submit-button" value="NEEUTRAL">
+            <input type="submit" class="submit-button" value="GENOCIDE">
+        </div>
+        <div class="mainimg">
+            <img src="assets/main.png" alt="main">
+        </div>
+    </div>
+</body>
+</html>
